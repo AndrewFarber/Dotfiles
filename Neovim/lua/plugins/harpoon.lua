ea7da36a5e0f126379331ed0991129edc3b2a08d
Andrew Farber <Andrew.Farber@outlook.com>
2024-11-17 17:31:53 -0800

    ♻️ Update Neovim harpoon plugin key maps

--- Neovim/lua/plugins/harpoon.lua
+++ Neovim/lua/plugins/harpoon.lua
@@ -1,12 +1,9 @@
-local harpoon = require("harpoon")
+-- GitHub Repository: https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+-- Description: Better marks
+local harpoon = require('harpoon')
 
 harpoon.setup({})
 
-vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-
-vim.keymap.set("n", "<F1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<F2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<F3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<F4>", function() harpoon:list():select(4) end)
+vim.keymap.set('n', '<leader>m', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Harpoon menu' })
+vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = 'Harpoon add' })
 
