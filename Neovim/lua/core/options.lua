d039ab7a434a49976fc31391617fee46648dbebf
Andrew Farber <Andrew.Farber@outlook.com>
2024-11-17 16:03:45 -0800

    ✨ Enable spell check

--- Neovim/lua/core/options.lua
+++ Neovim/lua/core/options.lua
@@ -20,7 +20,7 @@ opt.showmode = false        -- show mode below status bar
 opt.signcolumn = 'yes'      -- always include column left of line numbers
 opt.cursorline = true       -- highlight the text line of the cursor
 opt.scrolloff = 10          -- min number of screen lines to keep above and below the cursor
-opt.background = "dark"     -- colorschemes setting
+opt.background = "dark"     -- color schemes setting
 
 -- Line wrap
 opt.wrap = true             -- wrap text to next line
@@ -28,7 +28,7 @@ opt.breakindent = true      -- if text wraps, visually indent
 
 -- Searching
 opt.ignorecase = true       -- ignore case when searching
-opt.smartcase = true        -- if you include mix case in search, assume you want case-sensetive
+opt.smartcase = true        -- if you include mix case in search, assume you want case-sensitive
 
 -- Key press timing
 opt.timeoutlen = 400        -- mapped sequence time out (milliseconds)
@@ -37,12 +37,16 @@ opt.timeoutlen = 400        -- mapped sequence time out (milliseconds)
 opt.splitright = true       -- split vertical window to the right
 opt.splitbelow = true       -- split horizontal window to the bottom
 
--- Whitespace characters
-opt.list = true             -- update how whitespace characters are rendered
-opt.listchars = {           -- whitespace overrides
+-- White space characters
+opt.list = true             -- update how white space characters are rendered
+opt.listchars = {           -- white space overrides
     tab = '» ',
     trail = '·',
     nbsp = '␣',
     space = '·'
 }
+
+-- Spelling
+opt.spell = true            -- turn on spelling help
+opt.spelllang = 'en_us'     -- set spelling dictionary to US English
 
