32d286c37c82ac7ba2e6442afed36efa81e645d3
Andrew Farber <Andrew.Farber@outlook.com>
2024-11-19 16:17:34 -0800

    ♻️ Refactor Neovim lspconfig plugin

--- Neovim/lua/plugins/nvim-lspconfig.lua
+++ Neovim/lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,12 @@
+-- GitHub Repository: https://github.com/neovim/nvim-lspconfig
+-- Description: "Data only" repository providing basic Neovim LSP defaults
 local lspconfig = require('lspconfig')
 
--- Python LSP
+-- LSP configurations
 lspconfig.pyright.setup({})
 
--- Nix LSP
 lspconfig.nil_ls.setup({})
 
--- Lua LSP
 lspconfig.lua_ls.setup({
   settings = {
     Lua = {
@@ -22,20 +22,42 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
   callback = function(event)
 
+    local telescope = require('telescope.builtin')
+
     local map = function(keys, func, desc, mode)
       mode = mode or 'n'
-      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
     end
 
-    map('<leader>gd', require('telescope.builtin').lsp_definitions, 'Goto Definition')
-    map('<leader>gD', vim.lsp.buf.declaration, 'Goto Declaration')
-    map('<leader>gr', require('telescope.builtin').lsp_references, 'Goto References')
-    map('<leader>gi', require('telescope.builtin').lsp_implementations, 'Goto Implementation')
-    map('<leader>gt', require('telescope.builtin').lsp_type_definitions, 'Goto Type Definition')
-    map('<leader>gs', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
-    map('<leader>gS', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Workspace Symbols')
-    map('<leader>rn', vim.lsp.buf.rename, 'Rename')
-    map('<leader>ca', vim.lsp.buf.code_action, 'Code Action', { 'n', 'x' })
+    -- Jump to the definition of the word under your cursor
+    -- This is where a variable was first declared/defined/etc
+    map('<leader>gd', telescope.lsp_definitions, 'Goto Definition')
+
+    -- Find references for the word under your cursor
+    map('<leader>gr', telescope.lsp_references, 'Goto References')
+
+    -- Jump to the implementation of the word under your cursor
+    -- Useful when your language has ways of declaring types without an actual implementation
+    map('<leader>gi', telescope.lsp_implementations, 'Goto Implementation')
+
+    -- Jump to the type of the word under your cursor
+    -- Useful when your're not sure what type of variable is and you want to see
+    -- the definition of its *type*, not where it was *defined*
+    map('<leader>gt', telescope.lsp_type_definitions, 'Goto Type Definition')
+
+    -- Fuzzy find all the symbols in your current document
+    -- Symbols are things like variables, functions, types, etc.
+    map('<leader>gs', telescope.lsp_document_symbols, 'Document Symbols')
+
+    -- Fuzzy find all the symbols in your current workspace
+    -- Similar to document symbols, except searches over your entire project
+    map('<leader>gS', telescope.lsp_dynamic_workspace_symbols, 'Workspace Symbols')
+
+    -- Rename word under cursor
+    map('<leader>r', vim.lsp.buf.rename, 'Rename')
+
+    -- Code actions
+    map('<leader>c', vim.lsp.buf.code_action, 'Code Action', { 'n', 'x' })
 
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
