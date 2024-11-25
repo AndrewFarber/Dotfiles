500fe0909d9d12fe733a1ae69e87ac958164534b
Andrew Farber <Andrew.Farber@outlook.com>
2024-11-24 18:00:28 -0800

    ♻️ Add comment to Neovim plugin which-key

--- Neovim/lua/plugins/which-key.lua
+++ Neovim/lua/plugins/which-key.lua
@@ -1,1 +1,6 @@
-require('which-key').setup({})
+-- GitHub Repository: https://github.com/folke/which-key.nvim
+-- Description: Key map hints while you type
+local wk = require('which-key')
+
+wk.setup({})
+
