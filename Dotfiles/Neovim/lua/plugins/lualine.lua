846c737b155d82877cc05c1375ae2cc1d4ad295a
Andrew Farber <Andrew.Farber@outlook.com>
2025-01-03 10:53:03 -0800

    ♻️ Move Neovim plugin nvim-navic to winbar

--- Dotfiles/Neovim/lua/plugins/lualine.lua
+++ Dotfiles/Neovim/lua/plugins/lualine.lua
@@ -1,14 +1,14 @@
 -- GitHub Repository: https://github.com/nvim-lualine/lualine.nvim
 -- Description: Configurable Neovim statusline
 require('lualine').setup({
-  sections = {
+  winbar = {
     lualine_c = {
       {
         "navic",
-        color_correction = "dynamic", -- nil, "static" or "dynamic"
-        navic_opts = nil,
+        color_correction = nil,
+        navic_opts = nil
       }
     }
-  },
+  }
 })
 
