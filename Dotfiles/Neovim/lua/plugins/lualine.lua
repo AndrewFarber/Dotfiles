524ac5871e7da239f39e85918ef16955c6b57247
Andrew Farber <Andrew.Farber@outlook.com>
2025-01-03 11:10:20 -0800

    ♻️ Update Neovim plugin nvim-navic

--- Dotfiles/Neovim/lua/plugins/lualine.lua
+++ Dotfiles/Neovim/lua/plugins/lualine.lua
@@ -1,14 +1,17 @@
 -- GitHub Repository: https://github.com/nvim-lualine/lualine.nvim
 -- Description: Configurable Neovim statusline
 require('lualine').setup({
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
   winbar = {
-    lualine_c = {
-      {
-        "navic",
-        color_correction = nil,
-        navic_opts = nil
-      }
-    }
+    lualine_a = { "filename" },
+    lualine_c = { { "navic", color_correction = nil, navic_opts = nil } }
   }
 })
 
